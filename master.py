aa29b87acd237395550750702c5f602eedab5252
4869APTX <yhfaptx4869@gmail.com>
2017-08-24 00:00:33 +0800

    first test

---
+++ master.py
@@ -0,0 +1,13 @@
+# *-* coding :utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+import os
+import re
+import random
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+se = requests.session()
+
