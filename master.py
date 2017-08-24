2d6edc88a57aa15311e775bb4bb5c7fb39fa235c
4869APTX <yhfaptx4869@gmail.com>
2017-08-25 01:21:35 +0800

    添加get_url,get_mark_items,

--- master.py
+++ master.py
@@ -31,6 +31,7 @@ class Pixiv():
         self.return_to = 'https://www.pixiv.net'
         self.load_path = 'D:\Pixiv_mark'
 
+    #登录模块
     def login(self):
         post_key_html = se.get(self.base_url,headers=self.headers).text
         # 获取登录界面
@@ -45,7 +46,57 @@ class Pixiv():
         }
         se.post(self.login_url,data=data,headers=self.headers)
 
+    #获取url的页面
+    def get_html(self,url):
+        return se.get(url=url,header=self.header)
+
+
+    #获取收藏页中的图片信息,返回图片链接和是否为多图
+    def get_mark_items(self,html):
+        section_soup = BeautifulSoup(html,'lxml')
+        section_list = section_soup.find_all('li',attrs={'class':'image-item'})
+        #获取当前收藏页中所有图片信息
+        Elements = {}
+        for items in section_list:
+            Pic_url = self.main_url + items.find('a')['href']
+            if 'multiple' in items.find('a')['class']:
+                Elements[Pic_url] = 1
+            else:
+                Elements[Pic_url] = 0
+        return Elements
+        #返回一个元素
+
+    #获取收藏图片数量
+    def get_total(self):
+        Mark_HtmlSource = self.get_html(self.mark_url).text
+        #获取收藏页面的源代码
+        Pattern = re.compile('<span class="count-badge">(.*?)件</span>',re.S)
+        #comlile的正则匹配，re.S为完全匹配，返回一个对象模式
+        Total = re.search(pattern = Pattern,string = Mark_HtmlSource).group(1)
+        #search调用compile的规则查找，group(1)为匹配到的第一个字符
+        print u'一共有' + str(Total) + u'件收藏'
+        return Total
+
+    #获取图片主页的信息
+    def get_pic_info(self,url,flag):
+        if flag :
+            return 0
+        else:
+
+
+
+
+
     def work(self):
+        self.login(self)
+        Mark_Total = self.get_total(self)
+        Mark_page_num = int(Mark_Total)/20
+        #求出收藏页数
+
+        for page_num in Mark_page_num:
+            Elements = self.get_html(self,self.mark_url)
+            for Pic_url,flag  in Elements.items():
+                Pic_info = self.get_pic_info(Pic_url,flag)
 
 
 
@@ -54,7 +105,6 @@ def main():
     pixiv.pixiv_id=input('输入pixiv 账号：')
     pixiv.password=input('输入密码：')
     pixiv.work()
-
 
 
 if __name__ == '__main__':
