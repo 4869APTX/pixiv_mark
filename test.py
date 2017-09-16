39e6157b8371f9d9dd36e2bb3dd42d7a25d14fa6
4869APTX <yhfaptx4869@gmail.com>
2017-09-16 10:30:32 +0800

    test

---
+++ test.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+import  msvcrt
+import os
+import re
+import  sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+#
+# url = 'https://pixiv.net/member_illust.php?mode=medium&illust_id=50107872'
+# Pattern = re.compile('illust_id=(.*?)$',re.S)
+# Img_id = re.search(pattern=Pattern, string=url)
+#
+# print Img_id.group(1)
+for i in range(20,1,-1):
+    print i
+
+
