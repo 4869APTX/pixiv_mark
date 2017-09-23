2cc49e0909978114ab6e5bf1073470d16c7d506d
4869APTX <yhfaptx4869@gmail.com>
2017-09-23 19:29:02 +0800

    本地代理功能实装

--- master.py
+++ master.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 import os
 import re
 import random
+import datetime
 import sys
 import time
 import msvcrt
@@ -29,13 +30,26 @@ class Pixiv():
         }
         self.pixiv_id = ''
         self.password = ''
+        self.rank_url = 'https://www.pixiv.net/ranking.php?mode=daily'
         self.post_key = []
         self.return_to = 'https://www.pixiv.net'
-        self.path = 'D:\Pixiv_mark'
+        self.path = 'E:\Pixiv_mark'
+        self.rank_path = 'E:\Pixiv_rank'
+        self.proxy = False
+        self.proxy_host = {'https':'http://127.0.0.1:1080'}
 
     #登录模块
     def login(self):
-        post_key_html = se.get(self.base_url,headers=self.headers).text
+        try:
+
+            post_key_html = se.get(self.base_url,headers=self.headers).text
+        except:
+            print u'是否需要使用代理'
+            key = raw_input('Y/N')
+            if key =='Y':
+                self.proxy = True
+                post_key_html = se.get(self.base_url,headers = self.headers,proxies = self.proxy_host)
+
         # 获取登录界面
         post_key_soup = BeautifulSoup(post_key_html,'lxml')
         self.post_key = post_key_soup.find('input')['value']
@@ -46,12 +60,15 @@ class Pixiv():
             'return_to': self.return_to,
             'post_key': self.post_key
         }
-        se.post(self.login_url,data=data,headers=self.headers)
+        if self.proxy :
+            se.post(self.login_url,data=data,headers=self.headers,proxies = self.proxy_host)
+        else:
+            se.post(self.login_url,data=data,headers=self.headers)
 
     #获取url的页面
-    def get_html(self,url,proxy = None,num_entries = 5):
+    def get_html(self,url,num_entries = 5):
 
-        if proxy is None:
+        if self.proxy == False:
             try:
                 return se.get(url,headers = self.headers)
             except:
@@ -60,14 +77,29 @@ class Pixiv():
                     time.sleep(5)
                     return self.get_html(url,num_entries = num_entries-1)
                 else:
-                    print u'该网页无法连接'
+                    print u'开始使用代理'
+                    time.sleep(5)
+                    proxy ={}
+                    self.proxy = True
+                    return self.get_html(url)
 
+        else:
+            try:
+                proxy = {'http':'127.0.0.1'}
+                return se.get(url,headers = self.headers,proxies = self.proxy_host)
+            except:
+                if num_entries >0:
+                    print u'网页获取失败，5秒后尝试重连，剩余次数 '+str(num_entries)
+                    time.sleep(5)
+                    return self.get_html(url,num_entries = num_entries-1)
+                else:
+                    print u'使用代理失败'
 
 
 
     #获取收藏页中的图片信息,返回图片链接和是否为多图
-    def get_mark_items(self,html):
-        mark_html = self.get_html(html).text
+    def get_mark_items(self,url):
+        mark_html = self.get_html(url).text
         section_soup = BeautifulSoup(mark_html,'lxml')
         section_list = section_soup.find_all('li',attrs={'class':'image-item'})
         #获取当前收藏页中所有图片信息
@@ -83,7 +115,7 @@ class Pixiv():
 
     #获取收藏图片数量
     def get_total(self):
-        Mark_HtmlSource = str(se.get(url=self.mark_url).text)
+        Mark_HtmlSource = str(self.get_html(url=self.mark_url).text)
         # print Mark_HtmlSource
         #获取收藏页面的源代码
         Pattern = re.compile('<span class="count-badge">(.*?)件</span>',re.S)
@@ -146,11 +178,6 @@ class Pixiv():
             return 0
 
 
-
-
-
-
-
     def mkdir(self,path):
         self.path = self.path.strip()
         is_exist = os.path.exists(path=self.path)
@@ -165,7 +192,7 @@ class Pixiv():
             return False
 
 
-    def work(self):
+    def mark(self):
         self.login()
         print u'登录成功'
         # input('fuck')
@@ -182,7 +209,6 @@ class Pixiv():
         self.mkdir(self.path)
         raw_input('dir make success')
 
-
         for page_num in range(1,Mark_page_num + 1):
             Mark_page_html = self.mark_url + str(page_num)
             Elements = self.get_mark_items(Mark_page_html)
@@ -197,12 +223,34 @@ class Pixiv():
 
         print u'全部图片保存完毕'
 
+    def rank(self):
+        t = datetime.datetime.now()
+        time = str(t.year) + '_' + str(t.month) + '_' + str(t.day)
+
+        self.login()
+        path = self.rank_path +'\\' +  time
+        self.mkdir(path)
+        now_html = self.get_html(self.rank_url, 3)
+
+        self.get_img(now_html.text, time)
+        print time + u'文件夹已保存完毕'
+
 def main():
     pixiv = Pixiv()
-    pixiv.pixiv_id=raw_input(u'输入pixiv 账号：')
-    pixiv.password=raw_input(u'输入密码：')
 
-    pixiv.work()
+    # pixiv.pixiv_id=raw_input(u'输入pixiv 账号：')
+    # pixiv.password=raw_input(u'输入密码：')
+
+    key = ''
+    while (key != '1' or key !='2'):
+        print u'请输入需要爬取的页面：/n'
+        print u'1.收藏夹'
+        print u'2.每日排行榜(暂未实装)'
+        key = raw_input(u'输入数字')
+    if key =='1':
+        pixiv.mark()
+    else:
+        pixiv.rank()
 
 
 if __name__ == '__main__':
